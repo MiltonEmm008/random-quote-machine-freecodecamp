8a6ccccda9e834f44e03dbd36816094f54bed7f1
MiltonEmm008 <merp2067@gmail.com>
2025-07-03 23:22:31 -0700

    icono y url corregida

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/random-quote-machine-react",
+  base: "/random-quote-machine-freecodecamp",
 });
